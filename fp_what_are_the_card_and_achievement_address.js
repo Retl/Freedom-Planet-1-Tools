d66fba048d7bdfff91fae611850b6d7d9be43a5e
Carlis Moore <carlis.moore@gmail.com>
2020-09-20 04:00:48 -0500

    Add info on reproducing the correct pointers for other versions.

---
+++ fp_what_are_the_card_and_achievement_address.js
@@ -0,0 +1,71 @@
+//To run this> node fp_what_are_the_card_address.js
+// basePointerOffset should be a pointer to the first card. Should be able to get this with one offset from base.
+// basePointer First offset relative to the executable. Honestly I just end up finding this as a byproduct of 
+// pointer-scanning for the first card's status repeatedly as I grab it, quit, reset card status, and repeats.
+
+/*
+
+Process for finding the offset for cards with CE: Touch card 1. Scan for aligned 4byte int value of 1. Quit the level, go to the gallery, and delete the cards. Go back into DV and scan for value 0 any time before collecting the card. Then collect the card and scan for 1 again. Repeat this process until there are 10 or less values. At which point you can try freezing the values to 1 or 0, restarting the level, and walking to where the card is to see if it looks like it's collected or not based on what you froze.
+
+Once you're sure you've got the right value, do a pointer scan. Try to find a route with less than 3 additional pointers. Add a handful to your table, save the table, then restart the game and reattach. Confirm that the pointers are still valid at least while in-level. Sometimes these pointers bug out in menus.
+
+Every card pointer after that just adding 80 bytes to that pointer (not an extra pointer, it's a different pointer in a different spot) (0x50)
+--
+
+For Achievements, the fastest way is to: 
+Clear all achievements
+start Aqua Tunnel and immediately scan for 0
+Find the digging spot on the floor one level below AT's first card (this is like 5 seconds away from the starting area, just flutter up and to the right immediately, but not to the highest spot.
+Immediately after digging the spot up and seeing the treasure appear, search for int value 1.
+Quit the level. The Achievement screen should pop up. After it's visible on screen, scan for int value 2.
+Go to the Gallery menu and reset the achievements again. DO NOT SCAN ON THIS SCREEN, THIS TENDS TO BREAK FOR SOME REASON
+Restart AT again and scan for 0 again. Repeat until there are few results left (this shouldn't take long).
+
+To confirm you have the right one, you can't just force a 2 because Steam will verify your achievements and overwrite it. Instead, delete your achievements, start ANY level (even SMA), then set the achievement value to 1. Quit the level. On exit, the achievement should be "earned" and advance to 2 as expected.
+
+Once you've confirmed it, used the PointerScan as before to try to find a route with 3 or less bounces to the pointing address. Add a handful of them, because some of them will be wrong. Close the game. Open it again. Reattach to the process. Confirm that the achievement is still being tracked properly or can be manipuilated as before.
+
+However because the Digging Spot achievement is the 38th achievement, we need to work back to the first one (clearing as Lilac). As before, this is a 80 byte difference between each achievement (0x50). For this I just opened up the Windows Calculator, set it on hex mode, grabbed the last pointer value (in my case, B94) and subtracted B90 from it (0x50 * 0x25 or 80 * 37). The resulting pointer route should now be going to the first achievement. To find every achievement from there, just add the 0x50.
+
+Every Achievement pointer after that just adding 80 bytes to that pointer (not an extra pointer, it's a different pointer in a different spot) (0x50)
+
+*/
+
+var as = "50";
+var bs = "0x4C";
+var a = parseInt(as, 16); 
+var b = parseInt(bs, 16);
+var c = a + b;
+console.log(a);
+console.log(as);
+console.log(b);
+console.log(bs);
+console.log(c);
+console.log(c.toString(16));
+
+console.log("--------------------------------------------------");
+console.log("Card Addresses: ");
+// "fp.exe"+0148902C, 4C => Codex Capture Card 1: 4 byte value. 0 = Unobtained. 1 = Obtained. 2 = Opened.
+var basePointerOffset = "0x4C";
+var cardDelta = "0x50";
+var basePointer = "0x0148902C";
+// Output it in a way that's easy to copypaste into a Livesplit ASL script.
+for (i=0; i<100; i++) {
+    console.log('int card' + (i+1) +' : ' + basePointer + ', 0x' + (parseInt(basePointerOffset) + (parseInt(cardDelta) * i)).toString(16) + ';');
+    //console.log("Card " + (i+1))
+}
+console.log("--------------------------------------------------");
+
+console.log("--------------------------------------------------");
+console.log("Achievement Addresses: ");
+// "fp.exe"+0148902C, 4C => Codex Capture Card 1: 4 byte value. 0 = Unobtained. 1 = Obtained. 2 = Opened.
+var basePointerOffsetAchievements = "0x04";
+
+// Output it in a way that's easy to copypaste into a Livesplit ASL script.
+for (i=0; i<100; i++) {
+    console.log('int achievement' + (i+1) +' : ' + basePointer + ', 0x' + (parseInt(basePointerOffsetAchievements) + (parseInt(cardDelta) * i)).toString(16) + ';');
+    //console.log("Card " + (i+1))
+}
+console.log("--------------------------------------------------");
+
+//"fp.exe"+0148902C
